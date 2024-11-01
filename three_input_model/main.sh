9d0358dfcf8c2e19729c80e01d4a368b13f71784
Baradwajpeela <baradwajnaidu72@gmail.com>
2024-11-01 11:12:42 -0500

    added some files

--- three_input_model/main.sh
+++ three_input_model/main.sh
@@ -1,10 +1,10 @@
 mv input0 input
 mpiexec -np 8 vampire-parallel
 mv input input0
-mv input1 input
-mpiexec -np 8 vampire-parallel
-mv input input1
+#mv input1 input
+#mpiexec -np 8 vampire-parallel
+#mv input input1
 #mv input2 input
-#vampire-serial
+#mpiexec -np 8 vampire-parallel
 #mv input input2
 gnuplot -p plotfield.gp
