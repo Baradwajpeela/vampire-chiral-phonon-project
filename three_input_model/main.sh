1328a1cca4fad02bd0323d12c5523e8ba95135a3
Baradwajpeela <baradwajnaidu72@gmail.com>
2024-11-25 13:01:20 -0600

    still figuring out precconditioning

--- three_input_model/main.sh
+++ three_input_model/main.sh
@@ -1,9 +1,9 @@
 mv input_B+ input
-mpiexec -np 6 vampire-parallel
+mpiexec -np 10 vampire-parallel
 mv input input_B+
 cp output output_B+
 mv input_B- input
-mpiexec -np 6 vampire-parallel
+mpiexec -np 10 vampire-parallel
 mv input input_B-
 cp output output_B-
 gnuplot -p plot_B_M.gp
