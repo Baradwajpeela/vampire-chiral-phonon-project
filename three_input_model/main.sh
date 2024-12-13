6d1207299702c0af7a2592f2f77cb95ac40f585f
Baradwajpeela <baradwajnaidu72@gmail.com>
2024-12-13 14:07:36 -0600

    Laser power reduced

--- three_input_model/main.sh
+++ three_input_model/main.sh
@@ -1,17 +1,24 @@
 mv input_pre_pulse input
-mpiexec -np 8 vampire-parallel
+mpiexec -np 10 vampire-parallel
 mv input input_pre_pulse
 mv input_B+ input
-mpiexec -np 8 vampire-parallel
+mpiexec -np 10 vampire-parallel
 mv input input_B+
 cp output output_B+
 mv input_pre_pulse input
-mpiexec -np 8 vampire-parallel
+mpiexec -np 10 vampire-parallel
 mv input input_pre_pulse
 mv input_B- input
-mpiexec -np 8 vampire-parallel
+mpiexec -np 10 vampire-parallel
 mv input input_B-
 cp output output_B-
+mv input_pre_pulse input
+mpiexec -np 10 vampire-parallel
+mv input input_pre_pulse
+mv input_B_0 input
+mpiexec -np 10 vampire-parallel
+mv input input_B_0
+cp output output_B_0
 gnuplot -p plot_B_M.gp
 gnuplot -p plot_B_M_T.gp
 
