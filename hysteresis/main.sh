57bf3f7894bebd39fbcd9a22df125d6a8e7f1ad3
Baradwajpeela <baradwajnaidu72@gmail.com>
2025-01-17 13:16:49 -0600

    updating system size and demag fields

--- hysteresis/main.sh
+++ hysteresis/main.sh
@@ -1,2 +1,13 @@
+mv input_Nd input
 mpiexec -np 10 vampire-parallel
+mv input input_Nd
+mv output output_Nd
+mv input_Nd_off input
+mpiexec -np 10 vampire-parallel
+mv input input_Nd_off
+mv output output_Nd_off
+mv input_dipole input
+mpiexec -np 10 vampire-parallel
+mv input input_dipole
+mv output output_dipole
 gnuplot -p plotfield.gp
